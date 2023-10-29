573d6f09f3db643bb59f86a30db85768e95cda0d
Adem-BK <101715968+Adem-BK@users.noreply.github.com>
2023-10-29 21:47:30 +0100

    Add files via upload

---
+++ Profile/index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+
+    <!-- box icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+
+    <title>Med Adam BK</title>
+</head>
+<body>
+
+    <!-- header design -->
+    <header class="header">
+        <a href="#" class="logo">Adam</a>
+        <div class="home-sci">
+            <a href="https://www.facebook.com/adam.benkhalifa.37/" target="_blank"><i class='bx bxl-facebook' ></i></a>
+            <a href="mailto:m.benkhlifa0703@myu.universitecentrale.tn" target="_blank"><i class='bx bx-mail-send'></i></a>
+        </div>
+        <div class="bx bx-menu" id="menu-icon"></div>
+        <nav class="navbar">
+            <a href="#home" class="active">Acceuil</a>
+            <a href="#about">A Propos</a>
+            <a href="#education">Education</a>
+            <a href="#skills">Compétences</a>
+            <a href="#contact">Contact</a>
+
+            <sapn class="active-nav"></sapn>
+        </nav>
+    </header>
+
+    <!-- home section design -->
+    <section class="home" id="home">
+        <div class="home-content">
+            <h1>Je Suis Med. Adam BenKhlifa</h1>
+            <div class="text-animate">
+                <h3  class="text-animate">Génie Logiciel</h3>
+                <p>
+                    En tant que licenciée en génie logiciel, je suis une candidate idéale pour les entreprises à la recherche d'un talent technique. 
+                    Ma formation solide et mes compétences en développement logiciel, gestion de projets et analyse des besoins des utilisateurs en font une candidate polyvalente. 
+                    Je suis capable de résoudre des problèmes complexes, d'innover et de travailler en équipe. 
+                </p>
+            </div>
+            
+            <div class="btn-box">
+                <a href="#" class="btn">Embauchez-Moi</a>
+                <a href="#" class="btn">Engageons La Conversation</a>
+            </div>
+        </div>
+        <div class="home-imgHover">
+        </div>
+    </section>
+
+    <section class="about" id="about">
+        <h2 class="heading"><span id="heading-style">A</span> Propos</h2>
+
+        <div class="about-img">
+            <img src="IMAGES/square_profile_pic.png" alt="img">
+            <span class="circle-spin"></span>
+        </div>
+        <div class="about-content">
+            <h3>Génie Logiciel!</h3>
+            <p>
+                En tant que licenciée en génie logiciel, je suis dotée d'un ensemble complet de compétences qui font de moi une candidate idéale pour toute entreprise à la recherche d'un talent technique de premier ordre. 
+                Ma solide formation académique m'a permis de maîtriser les principes fondamentaux du génie logiciel, ainsi que les langages de programmation les plus couramment utilisés dans l'industrie. 
+                Mon expertise s'étend à la conception et au développement de logiciels / Sites Web, à la gestion de projets informatiques et à l'analyse des besoins des utilisateurs. 
+                Je possède également une solide expérience pratique dans la résolution de problèmes complexes et la création de solutions innovantes. Grâce à ma capacité à travailler en équipe et à communiquer efficacement, je suis en mesure de collaborer avec des professionnels multidisciplinaires pour offrir des résultats exceptionnels. Mon désir constant de rester à la pointe des dernières avancées technologiques me permet de m'adapter rapidement à de nouvelles technologies et de fournir des solutions adaptées aux besoins spécifiques de chaque entreprise. Avec mon dévouement, ma passion et ma volonté de relever de nouveaux défis, je suis convaincue que je serai un atout précieux pour votre entreprise.
+            </p>
+        </div>
+        <div class="btn-box btns">
+            <a href="#" class="btn">Lire Encore</a>
+        </div>
+    </section>
+
+        <!-- Education section design -->
+    <section class="education" id="education">
+        <h2 class="heading">Ma <span id="heading-style">Journée</span></h2>
+        <div class="education-row">
+            <div class="education-column">
+                <h3 class="title">Education</h3>
+                <div class="education-box">
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class='bx bx-calendar'></i> 2021 - 2024</div>
+                            <h3>Licence Appliquée En Génie Logiciel Et Système d'Information | Université Centrale</h3>
+                            <p>
+                                En tant que licenciée en Génie Logiciel et Systèmes d'Information (GLSI), je suis une professionnelle passionnée et compétente dans la conception et le développement de solutions informatiques innovantes. Ma maîtrise des langages de programmation tels que Java, Python et C++, ainsi que ma familiarité avec les technologies web et les bases de données, me permettent de créer des applications robustes et conviviales. En travaillant de manière autonome ou en équipe, je suis motivée, adaptable et je m'efforce de rester à la pointe de mon domaine en cherchant constamment à acquérir de nouvelles connaissances. Mon approche méthodique, ma capacité à résoudre les problèmes et ma passion pour l'impact positif des technologies me permettent de contribuer de manière significative à des projets qui font la différence. Vous pouvez découvrir mes réalisations et compétences dans mon portfolio, où je suis ouvert à de nouvelles opportunités et collaborations passionnantes dans le domaine du génie logiciel et des systèmes d'information.
+                            </p>
+                        </div>
+                    </div>
+    
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class='bx bx-calendar'></i> 2020 - 2021</div>
+                            <h3>Baccalauréat - Informatique - Science de l'Informatique | Lycée Ibn Rachik</h3>
+                            <p>
+                                En tant que titulaire d'un baccalauréat en Informatique - Sciences de l'Informatique, je suis un professionnel passionné et compétent dans le domaine de l'informatique et des sciences de l'information. Ma formation approfondie m'a fourni une solide base de connaissances en programmation, en analyse des systèmes et en résolution de problèmes complexes. Je suis un expert en PHP, JavaScript, HTML, CSS et Pascal, capables de créer des applications web interactives et conviviales. Mon excellente maîtrise de l'algorithmique me permet de résoudre efficacement des problèmes et de développer des solutions optimales. J'ai acquis une expérience pratique grâce à mes projets académiques, ce qui m'a permis de développer des compétences solides en développement d'applications web, en création d'interfaces utilisateur attrayantes et en manipulation de données. Je suis un esprit curieux, créatif et méthodique, toujours à la recherche de nouvelles technologies et de solutions innovantes. Mon portfolio reflète mes réalisations et compétences en informatique, et je suis ouvert à de nouvelles opportunités et collaborations dans ce domaine dynamique.                        
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="education-column">
+                <h3 class="title">Expériences</h3>
+                <div class="education-box">
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class='bx bx-devices'></i> Développeur Cross-Platform</div>
+                            <h3></h3>
+                            <p>
+                                En tant que développeur cross-plateforme, je suis un professionnel spécialisé dans la création d'applications logicielles qui fonctionnent de manière transparente sur différentes plateformes et systèmes d'exploitation. Je possède une expertise approfondie dans le développement multiplateforme, ce qui me permet de concevoir et de mettre en œuvre des solutions logicielles efficaces et compatibles, que ce soit sur Windows, macOS, Linux, iOS ou Android. Je suis compétent dans l'utilisation de langages de programmation polyvalents tels que Java, C++, Python et JavaScript, ainsi que dans l'utilisation de frameworks et d'outils de développement adaptés à la création d'applications compatibles avec plusieurs plateformes. Mon expérience pratique et ma connaissance des meilleures pratiques en matière de développement cross-plateforme me permettent de fournir des applications performantes, conviviales et cohérentes sur différents appareils et systèmes d'exploitation. Je suis passionné par les défis techniques liés à l'interopérabilité et je suis constamment à la recherche de nouvelles solutions pour offrir des expériences utilisateur fluides et homogènes. En tant que développeur cross-plateforme, je suis prêt à relever de nouveaux défis et à contribuer de manière significative au développement d'applications multiplateformes.
+                            </p>
+                        </div>
+                    </div>
+    
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class='bx bx-code-alt'></i> Développeur Fullstack Web</div>
+                            <p>
+                                En tant que développeur fullstack web, je suis un professionnel polyvalent et passionné par la création d'applications web complètes. Fort de mon expertise tant dans le développement front-end que back-end, je suis capable de concevoir et de mettre en œuvre des solutions complètes, allant de l'interface utilisateur attrayante à la logique complexe des fonctionnalités côté serveur. Du côté front-end, je maîtrise les langages tels que HTML, CSS et JavaScript, ainsi que les frameworks comme React, Angular ou Vue.js, pour créer des interfaces utilisateur interactives et réactives. Du côté back-end, je suis à l'aise avec des langages tels que Java, Python, PHP, ainsi que des frameworks populaires tels que Node.js et Django, pour développer des fonctionnalités robustes, gérer les bases de données et mettre en place des API sécurisées. Je suis également compétent dans l'utilisation des bases de données SQL et NoSQL. Mon expérience pratique m'a permis de travailler sur une variété de projets web, du développement d'applications sur mesure aux sites e-commerce complexes. Je suis autonome, créatif et rigoureux dans mon approche de résolution de problèmes. Je suis également constamment à l'affût des dernières tendances et technologies du web, afin de fournir des solutions innovantes et performantes. Grâce à ma capacité à travailler en équipe, à communiquer efficacement et à respecter les délais, je suis prêt à relever de nouveaux défis et à contribuer de manière significative à des projets web ambitieux.                        
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Compétances section design -->
+    <section class="skills" id="skills">
+        <h2 class="heading">Mes <span id="heading-style">Compétances</span></h2>
+        
+        <div class="skills-row">
+            <div class="skills-column">
+                <h3 class="title">Coding</h3>
+
+                <div class="skills-box">
+                    <div class="skills-content">
+                        <div class="progress">
+                            <h3>HTML <span id="heading-style">85%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>CSS <span id="heading-style">95%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>JavaScript <span id="heading-style">75%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>C++ <span id="heading-style">80%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>C <span id="heading-style">85%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>Java <span id="heading-style">70%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>PHP <span id="heading-style">55%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>Python <span id="heading-style">70%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>Pascal <span id="heading-style">95%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>C# <span id="heading-style">80%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="skills-column">
+                <h3 class="title">Domaines</h3>
+
+                <div class="skills-box">
+                    <div class="skills-content">
+                        <div class="progress">
+                            <h3>Front-End Web<span id="heading-style">85%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>Back-End Web<span id="heading-style">60%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>développement Logiciels <span id="heading-style">75%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>Administration BDD <span id="heading-style">80%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>Conception Et modélisation des Logiciels <span id="heading-style">90%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section design -->
+    <section class="contact" id="contact">
+        <h2 class="heading"><span>Contactez</span> Moi!</h2>
+
+        <form action="#">
+            <div class="input-box">
+                <div class="input-field">
+                    <input type="text" placeholder="Nom Complet" required>
+                    <span class="focus"></span>
+                </div>
+                <div class="input-field">
+                    <input type="text" placeholder="Adresse Email" required>
+                    <span class="focus"></span>
+                </div>
+            </div>
+
+            <div class="input-box">
+                <div class="input-field">
+                    <input type="number" placeholder="Numéro téléphone" required>
+                    <span class="focus"></span>
+                </div>
+                <div class="input-field">
+                    <input type="text" placeholder="Sujet" required>
+                    <span class="focus"></span>
+                </div>
+            </div>
+
+            <div class="textarea-field">
+                <textarea name="" id="" cols="30" rows="10" placeholder="Ton Message..."></textarea>
+                <span class="focus"></span>
+            </div>
+
+            <div class="btn-box btns">
+                <button type="submit" class="btn">Envoyer</button>
+            </div>
+        </form>
+    </section>
+
+    <!-- Footer design -->
+    <footer class="footer">
+        <div class="footer-text">
+            <p>Copyright &copy; 2023 Par Mohammed Adam Ben Khlifa | Tous Les Droits Sont Réservés.</p>
+        </div>
+        <div class="footer-iconTop">
+            <a href="#"><i class='bx bxs-up-arrow-square' ></i></a>
+        </div>
+    </footer>
+    <script src="JS/script.js"></script>
+</body>
+</html>
